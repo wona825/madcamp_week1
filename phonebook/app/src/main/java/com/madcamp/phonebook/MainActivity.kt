abaaf7ab6f0649162a58d67af89fead3c6aa859b
chajiwon <wona825@naver.com>
2023-12-28 17:22:45 +0900

    add: tab layout 구현

--- phonebook/app/src/main/java/com/madcamp/phonebook/MainActivity.kt
+++ phonebook/app/src/main/java/com/madcamp/phonebook/MainActivity.kt
@@ -3,44 +3,140 @@ package com.madcamp.phonebook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.*
 import com.madcamp.phonebook.ui.theme.PhonebookTheme
+import com.madcamp.phonebook.ui.theme.greenColor
+import com.google.accompanist.pager.*
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PhonebookTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                TabLayout()
             }
         }
     }
 }
 
+@ExperimentalPagerApi
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TabLayout() {
+
+    val pagerState = rememberPagerState(pageCount = 3)
+
+    Column(
+        modifier = Modifier.background(Color.White)
+    ) {
+        TopAppBar(backgroundColor = greenColor) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Tab Layout",
+                    style = TextStyle(color = Color.White),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = TextUnit(
+                        18F,
+                        TextUnitType.Sp
+                    ),
+                    modifier = Modifier.padding(all = Dp(5F)),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+        Tabs(pagerState = pagerState)
+        TabsContent(pagerState = pagerState)
+    }
 }
 
-@Preview(showBackground = true)
+@ExperimentalPagerApi
 @Composable
-fun GreetingPreview() {
-    PhonebookTheme {
-        Greeting("Android")
+fun Tabs(pagerState: PagerState) {
+    val list = listOf(
+        "Friends" to Icons.Default.Call,
+        "Favorites" to Icons.Default.Favorite,
+        "Settings" to Icons.Default.Settings
+    )
+    val scope = rememberCoroutineScope()
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        backgroundColor = greenColor,
+        contentColor = Color.White,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
+                height = 2.dp,
+                color = Color.White
+            )
+        }
+    ) {
+        list.forEachIndexed { index, _ ->
+            Tab(
+                icon = {
+                    Icon(imageVector = list[index].second, contentDescription = null)
+                },
+                text = {
+                    Text(
+                        list[index].first,
+                        color = if (pagerState.currentPage == index) Color.White else Color.LightGray
+                    )
+                },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun TabsContent(pagerState: PagerState) {
+    HorizontalPager(state = pagerState) {
+            page ->
+        when (page) {
+            0 -> TabContentScreen(data = "Welcome to Screen 1")
+            1 -> TabContentScreen(data = "Welcome to Screen 2")
+            2 -> TabContentScreen(data = "Welcome to Screen 3")
+        }
+    }
+}
+
+@Composable
+fun TabContentScreen(data: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = data,
+            style = MaterialTheme.typography.h5,
+            color = greenColor,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
     }
 }
