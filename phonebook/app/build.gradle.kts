a549fa43b979809ecc12810ed09165a12c502770
chajiwon <wona825@naver.com>
2024-01-02 13:23:21 +0900

    fix: delete no usage of dependency

--- phonebook/app/build.gradle.kts
+++ phonebook/app/build.gradle.kts
@@ -87,7 +87,5 @@ dependencies {
     implementation("io.ktor:ktor-client-serialization:1.6.4")
     implementation("io.ktor:ktor-client-logging:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
-
-    implementation("com.typesafe:config:1.4.1")
 }
 
