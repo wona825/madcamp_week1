4946011c8af3a4260ba15afdbe226253fb8da660
sgn08050 <sgn08050@kaist.ac.kr>
2024-01-02 15:47:29 +0900

    feat: object change

---
+++ phonebook/app/src/main/java/com/madcamp/phonebook/presentation/gallery/favorites/favoritesClass.kt
@@ -0,0 +1,14 @@
+package com.madcamp.phonebook.presentation.gallery.favorites
+
+import android.net.Uri
+import com.madcamp.phonebook.domain.model.Contact
+
+data class favorites (
+    var name: String,
+    var icon: Int,
+    val image: Uri?,
+    var love: Boolean,
+    var description: String,
+    var valid: Boolean,
+    var dateTime: String?
+)
