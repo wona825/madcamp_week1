a33cbea448c8bbd5bd7d1ccccc8a6514f4979f24
chajiwon <wona825@naver.com>
2024-01-02 13:21:12 +0900

    feat: connect to server for contact function

--- phonebook/app/src/main/java/com/madcamp/phonebook/presentation/contact/viewmodel/ContactViewModel.kt
+++ phonebook/app/src/main/java/com/madcamp/phonebook/presentation/contact/viewmodel/ContactViewModel.kt
@@ -1,12 +1,48 @@
-package com.madcamp.phonebook.presentation.contact.viewmodel
+package com.madcamp.phonebook.presentation.contact.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.madcamp.phonebook.domain.model.Contact
+import com.madcamp.phonebook.domain.response.ContactResponse
+import com.madcamp.phonebook.presentation.contact.repository.ContactRepository
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
-class ContactViewModel : ViewModel() {
+class ContactViewModel: ViewModel() {
     var phoneCallListener: PhoneCallListener? = null
     var sendMessageListener: SendMessageListener? = null
+
+    var contactList by mutableStateOf<List<Contact>>(emptyList())
+
+    private val contactFLow = MutableStateFlow<List<ContactResponse>>(listOf())
+
+    fun event() {
+        viewModelScope.launch() {
+            kotlin.runCatching {
+                ContactRepository.fetchUsers(contactList)
+            }.onSuccess {
+                contactFLow.value = it
+                contactList = convertToContactList(contactFLow.value)
+            }.onFailure {
+                contactFLow.value = emptyList()
+            }
+        }
+    }
+
+
+    private fun convertToContactList(contactResponses: List<ContactResponse>): List<Contact> {
+        return contactResponses.map {
+            Contact(
+                name = it.name,
+                phoneNumber = it.phoneNumber,
+                favoriteStatus = it.favoriteStatus
+            )
+        }
+    }
+
     interface PhoneCallListener {
         fun makePhoneCall(phoneNumber: String)
     }
